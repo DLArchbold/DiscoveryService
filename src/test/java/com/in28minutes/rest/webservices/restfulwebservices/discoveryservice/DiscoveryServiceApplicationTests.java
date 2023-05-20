594b66305a708f00813e1b79fdf87903e8e288f7
Wei Jin Kok <wkok3@gatech.edu>
2023-05-20 18:29:14 -0400

    Working Eureka discovery service for personal project

---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
